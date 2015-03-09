edefd383baba1bb6941ba4c2156f07790952dbef
Fukuo Kadota <app.fk91@gmail.com>
2015-03-09 15:34:18 +0900

    let's responsive

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module OpenChecker
   class Application < Rails::Application
     config.generators.template_engine = :slim
-
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
